0ef34cbfbe40f898d9a15769625e9cb24e5c01ff
Sean King <seanking@Seans-MacBook-Pro-2.local>
2019-10-14 23:52:28 -0400

    New menu for the Main.py, can be built out for more functions.

--- Main.py
+++ Main.py
@@ -5,10 +5,17 @@
 from Game import *
 
 def main():
-
-  new_Game = Game("game_instance_3.txt")
-  print(new_Game.findPureNash())
-  q,p=new_Game.findMixedNash()
-  print("mixed nash: ", q,p)
+    filename = input("Please input the name of the file that you want to test: ")
+    new_Game = Game(filename)
+    exit = False
+    while not exit:
+        choice = input("Do you want to test for (A) pure Nash equilibria or (B) mixed Nash equilibria?  Or, type exit to end the program.")
+        if choice.lower() == "a":
+            print(new_Game.findPureNash())
+        elif choice.lower() == "b":
+            q,p=new_Game.findMixedNash()
+            print("mixed nash: ", q,p)
+        elif choice.lower() == "exit":
+            exit = True
 
 main()
