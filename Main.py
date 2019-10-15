0626be863945ce12cb72a43b4cf7529184729cbd
Sean King <seanking@Seans-MacBook-Pro-2.local>
2019-10-15 10:59:55 -0400

    More menu options

--- Main.py
+++ Main.py
@@ -9,12 +9,17 @@ def main():
     new_Game = Game(filename)
     exit = False
     while not exit:
-        choice = input("Do you want to test for (A) pure Nash equilibria or (B) mixed Nash equilibria?  Or, type exit to end the program.")
+        print("Do you want to test for (A) pure Nash equilibria or (B) mixed Nash equilibria")
+        choice = input("(C) Find dominant strategies, (D) Find dominated strategies?  Or, type exit to end the program.")
         if choice.lower() == "a":
             print(new_Game.findPureNash())
         elif choice.lower() == "b":
             q,p=new_Game.findMixedNash()
             print("mixed nash: ", q,p)
+        elif choice.lower() == "c":
+            new_Game.findDominant()
+        elif choice.lower() == "d":
+            new_game.findDominated()
         elif choice.lower() == "exit":
             exit = True
 
